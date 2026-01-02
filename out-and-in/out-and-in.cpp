46704f57f4c4b2d7b020f56ed8d24db868da9cd9
Lana Vu <lananghivu@gmail.com>
2026-01-01 16:25:57 -0800

    implemented hashmap menu test

---
+++ out-and-in/out-and-in.cpp
@@ -0,0 +1,22 @@
+#include "out-and-in.hpp"
+#include <iostream>
+
+#include <unordered_map>
+//defn
+
+int main () {
+    std::unordered_map<std::string, int> OutMenu;
+
+    OutMenu["Cheezburger"] = 5;
+    OutMenu["Burger"] = 5;
+    OutMenu["Fries"] = 2;
+    OutMenu["Soda"] = 2;
+    OutMenu["Smoothie"] = 3;
+    OutMenu["Hot Chocolate"] = 2;
+
+    std::cout << "This is our menu:";
+    for (auto i : OutMenu) {
+        std::cout << i.first << " " << i.second << std::endl;
+        return 0;
+    }
+}
---
+++ out-and-in/out-and-in.hpp
@@ -0,0 +1,14 @@
+#ifndef OUT-AND-IN_HPP
+#define OUT-AND-IN_HPP
+
+#include <iostream>
+
+
+//bp
+
+class outandin {
+    public:
+
+}
+
+#endif
