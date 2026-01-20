f3d4dfbd64747e22e90df84f4649ebc3c4f003a3
Lana Vu <lanavu@csu.fullerton.edu>
2026-01-19 16:13:32 -0800

    mighth losose connection on mountain, still same as prev

--- Stores/RickRonalds.cpp
+++ Stores/RickRonalds.cpp
@@ -4,6 +4,7 @@
 #include <vector>
 #include <unordered_map>
 
+rickOrder::
 void rickPath() {
     std::unordered_map<std::string, double> rickMenu;
 
--- Stores/RickRonalds.hpp
+++ Stores/RickRonalds.hpp
@@ -5,11 +5,16 @@
 
 class rickOrder {
     public:
-
+    rickOrder();
     std::string npcOrdering();
     std::string npcLines();
 
     private:
+    std::vector<std::string> burger_;
+    std::vector<std::string> nuggets_;
+    std::vector<std::string> fries_;
+    std::vector<std::string> drink_;
+    bool iceCream_;
     std::vector<std::string> order_;
 
 };
